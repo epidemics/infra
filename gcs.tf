f72e23173862a564847a7794b1423a4e7902630d
Daniel Hnyk <hnykda@gmail.com>
2020-04-02 21:29:01 +0200

    bucket

--- gcs.tf
+++ gcs.tf
@@ -19,3 +19,28 @@ resource "google_storage_bucket" "static-covid" {
     ]
   }
 }
+
+resource "google_storage_bucket" "epidemics-covid" {
+  bucket_policy_only = false
+  force_destroy      = false
+  location           = "US"
+  name               = "epidemics-covid"
+  requester_pays     = false
+  storage_class      = "MULTI_REGIONAL"
+
+  cors {
+    method          = ["GET", "HEAD"]
+    max_age_seconds = 60
+
+    origin = [
+      "http://epidemicforecasting.org",
+      "http://staging.epidemicforecasting.org",
+      "https://epidemicforecasting.org",
+      "https://staging.epidemicforecasting.org",
+    ]
+    response_header = [
+      "Content-Type",
+    ]
+  }
+}
+
